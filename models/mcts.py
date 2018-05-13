0a7577fb2973da22c8775643e7448da5bb09a29c
dylan djian <dylandjian@gmail.com>
2018-05-13 11:25:53 +0200

    [MCTS] Final fix on multithreading

--- models/mcts.py
+++ models/mcts.py
@@ -91,32 +91,31 @@ class EvaluatorThread(threading.Thread):
 
 
     def run(self):
-        for sim in range(MCTS_SIM // BATCH_SIZE_EVAL):
+        for sim in range(MCTS_SIM // MCTS_PARALLEL):
 
             ## Wait for the eval_queue to be filled by new positions to evaluate
             self.condition_search.acquire()
-            while len(self.eval_queue) < BATCH_SIZE_EVAL or \
-                  sim == MCTS_PARALLEL // BATCH_SIZE_EVAL - 1 and \
-                  len(self.eval_queue) != MCTS_PARALLEL - BATCH_SIZE_EVAL:
+            while len(self.eval_queue) < MCTS_PARALLEL:
                 self.condition_search.wait()
             self.condition_search.release()
 
             self.condition_eval.acquire()
-            keys = list(self.eval_queue.keys())
-            max_len = BATCH_SIZE_EVAL if len(keys) > BATCH_SIZE_EVAL else len(keys)
+            while len(self.result_queue) < MCTS_PARALLEL:
+                keys = list(self.eval_queue.keys())
+                max_len = BATCH_SIZE_EVAL if len(keys) > BATCH_SIZE_EVAL else len(keys)
 
-            ## Predict the feature_maps, policy and value
-            states = torch.tensor(np.array(list(self.eval_queue.values()))[0:max_len],
-                        dtype=torch.float, device=DEVICE)
-            v, probas = self.player.predict(states)
+                ## Predict the feature_maps, policy and value
+                states = torch.tensor(np.array(list(self.eval_queue.values()))[0:max_len],
+                            dtype=torch.float, device=DEVICE)
+                v, probas = self.player.predict(states)
 
-            ## Replace the state with the result in the eval_queue
-            ## and notify all the threads that the result are available
-            for idx, i in zip(keys, range(max_len)):
-                del self.eval_queue[idx]
-                self.result_queue[idx] = (probas[i].cpu().data.numpy(), v[i])
+                ## Replace the state with the result in the eval_queue
+                ## and notify all the threads that the result are available
+                for idx, i in zip(keys, range(max_len)):
+                    del self.eval_queue[idx]
+                    self.result_queue[idx] = (probas[i].cpu().data.numpy(), v[i])
 
-            self.condition_eval.notifyAll()
+                self.condition_eval.notifyAll()
             self.condition_eval.release()
 
 
